6170107606ceb18958ee4b2dcfc2131f84465d72
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-04-13 17:05:05 -0500

    Create authors

--- api/books/serializers.py
+++ api/books/serializers.py
@@ -5,3 +5,9 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ('id', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Show` instance, given the validated data.
+        """
+        return Author.objects.create(**validated_data)
