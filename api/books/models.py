7af28b786a8b3fc60dbc67802795ab9c014dac27
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-04-13 16:30:18 -0500

    Create book and author models

--- api/books/models.py
+++ api/books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    publisher = models.CharField(max_length=100, blank=False, null=False)
+    published_date = models.DateField(null=False)
+    description = models.TextField(blank=True, null=True, default='')
+    language = models.CharField(max_length=2, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    deleted_at = models.DateTimeField(auto_now=False, null=True)
+
+    class Meta:
+        ordering = ('created_at',)
+
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    deleted_at = models.DateTimeField(auto_now=False, null=True)
+
+    class Meta:
+        ordering = ('created_at',)
