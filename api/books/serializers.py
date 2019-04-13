4c180d6d31833de110ad5a42ecb665dd2462268e
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-04-13 18:35:10 -0500

    Serialize author data with books data

--- api/books/serializers.py
+++ api/books/serializers.py
@@ -1,10 +1,21 @@
+
 from rest_framework import serializers
 from books.models import Author, Book
 
 class BookSerializer(serializers.ModelSerializer):
+    class AuthorBookSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Author
+            fields = ('id', 'first_name', 'last_name')
+    
+    _author = AuthorBookSerializer(source='author', many=False, read_only=True)
+    
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'publisher', 'published_date', 'description', 'language')
+        fields = ('id', 'title', 'author', '_author', 'publisher', 'published_date', 'description', 'language')
+        extra_kwargs = {
+            'author': {'write_only': True}
+        }
 
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
