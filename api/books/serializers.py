4e4fb00dbea5e6755cf802d8e6e88fcd02d4926d
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-04-13 16:51:45 -0500

    Create author serializer

---
+++ api/books/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from books.models import Author
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('id', 'first_name', 'last_name')
