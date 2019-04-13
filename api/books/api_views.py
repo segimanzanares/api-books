46f5a260984dd4d76c34155a97f090ee8ed7f1fe
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-04-13 17:12:41 -0500

    Retrieve author details

--- api/books/api_views.py
+++ api/books/api_views.py
@@ -1,5 +1,7 @@
-from books.models import Author
+
+from books.models import Author, Book
 from books.serializers import AuthorSerializer
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -12,3 +14,15 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.filter(deleted_at=None)
     serializer_class = AuthorSerializer
 
+    """
+    Retrieve a model instance.
+    """
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        include = request.GET.get('include', '')
+        if include != '':
+            include = include.split(',')
+            if 'books' in include:
+                instance.books = Book.objects.filter(author=instance)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
