ee2d9f29bd1cf7cef97e1c127b8347e58c48be0c
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-04-13 17:28:51 -0500

    Fix serializer

--- api/books/serializers.py
+++ api/books/serializers.py
@@ -1,18 +1,20 @@
 from rest_framework import serializers
 from books.models import Author, Book
 
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id', 'title', 'author', 'publisher', 'published_date', 'description', 'language')
+
 class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
     class Meta:
         model = Author
-        fields = ('id', 'first_name', 'last_name')
+        fields = ('id', 'first_name', 'last_name', 'books')
 
     def create(self, validated_data):
         """
         Create and return a new `Show` instance, given the validated data.
         """
         return Author.objects.create(**validated_data)
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ('id', 'title', 'author', 'publisher', 'published_date', 'description', 'language')
